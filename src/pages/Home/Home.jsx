bcb434c8f476f10c525e4c11b015abf432381735
Nicolaslucero01 <103524887+Nicolaslucero01@users.noreply.github.com>
2023-05-08 17:09:49 -0300

    Lucero (#4)
    * Nosotros
    
    * Nosotros terminado
    
    * Cambios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./home.css";
 import { FaArrowRight } from "react-icons/fa";
 import { Link as Anchor } from "react-router-dom";
-import Oficina1 from "../../images/Oficina1.png"
-import Oficina2 from "../../images/Oficina2.png"
+import Oficina1 from "../../images/Oficina1.png";
+import Oficina2 from "../../images/imagen01.png";
 
 export default function Home() {
   return (
     <div className="HomeContainer">
-      
       <div className="home-tittle">
         <h1>
           CB <span>Global.</span>
@@ -32,14 +31,13 @@ export default function Home() {
       </div>
 
       <div className="content-images-home">
-        <div className="image-one">
-          <img src={Oficina1}  alt="Imagen de oficina numero 2" />
+        <div className="image">
+          <img src={Oficina2} alt="Imagen de oficina numero 2" />
         </div>
-        <div className="image-two">
-          <img src={Oficina2}  alt="Imagen de oficina numero 2" />
+        <div className="image">
+          <img src={Oficina2} alt="Imagen de oficina numero 2" />
         </div>
       </div>
-
     </div>
   );
 }
